cb31c7832042a75eeaeff64940dab73eb294df62
Imran Kabir <ikr22015@gmail.com>
2018-03-13 23:20:14 -0400

    Hooked up with genkins CI

--- HeaderMOD/src/main/java/headerPageObject/Search.java
+++ HeaderMOD/src/main/java/headerPageObject/Search.java
@@ -1,4 +1,13 @@
 package headerPageObject;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Search {
+    @FindBy(css = "#main-container > div > section.col-one > article:nth-child(1) > div > ul > li:nth-child(1) > a > div.quicklinks_list__detail > span")
+    WebElement start;
+
+    public void sart(){
+        start.click();
+    }
 }
--- HeaderMOD/src/test/java/headerTestCases/SearchTest.java
+++ HeaderMOD/src/test/java/headerTestCases/SearchTest.java
@@ -1,4 +1,14 @@
 package headerTestCases;
 
-public class SearchTest {
+import base.CommonAPI;
+import headerPageObject.Search;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+public class SearchTest extends CommonAPI{
+    @Test
+    public void testStart(){
+        Search search = PageFactory.initElements(driver, Search.class);
+        search.sart();
+    }
 }
